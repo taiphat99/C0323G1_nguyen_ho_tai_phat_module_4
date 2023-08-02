1d9f6aad8408a97ec5f7c0c135a4adca81ac33da
taiphat99 <taiphatlqp@gmail.com>
2023-08-02 15:42:47 +0700

    update blog

--- ss6/blog/src/main/java/com/blog/controller/RestBlogController.java
+++ ss6/blog/src/main/java/com/blog/controller/RestBlogController.java
@@ -8,14 +8,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("api-blog")
 public class RestBlogController {
     @Autowired
@@ -29,4 +26,6 @@ public class RestBlogController {
         }
         return new ResponseEntity<>(blogPage,HttpStatus.OK);
     }
+
+
 }
