225a8e8dfbd5cc6fd088b177f13874d31f933b2d
taiphat99 <taiphatlqp@gmail.com>
2023-08-03 17:26:26 +0700

    ss12

---
+++ s11/security/src/main/java/com/security/repository/AppUserRepository.java
@@ -0,0 +1,8 @@
+package com.security.repository;
+
+import com.security.model.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppUserRepository extends JpaRepository<AppUser,Long> {
+    AppUser findByUserName(String userName);
+}
---
+++ s11/security/src/main/java/com/security/repository/UserRoleRepository.java
@@ -0,0 +1,11 @@
+package com.security.repository;
+
+import com.security.model.AppUser;
+import com.security.model.UserRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
+    List<UserRole> findByAppUser(AppUser appUser);
+}
